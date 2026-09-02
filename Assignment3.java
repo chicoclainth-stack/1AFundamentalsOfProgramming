f15f038be537ef8295ddc40a26231fb50e24b037
Rojel123 <26ur084@psu.edu.ph>
2026-09-02 21:16:04 +0800

    chico malaki ulo

---
+++ Assignment3.java
@@ -0,0 +1,18 @@
+public class Assignment3 {
+    public static void main(String[] args) {
+
+        String firstName = "Clainth Javern"; // Stores the first name as a string.
+        String lastName = "Chico"; // Stores the last name as a string.
+        String address = "Pinmaludpod, Urdaneta City"; // Stores the address as a string.
+        String mobileNumber = "09776978676"; // Stores the mobile number as a string to keep the leading zero.
+        Integer Age = 17; // Stores the age as an integer because it is a whole number.
+        double gwa = 92.40; //Store the GWA as a double because it has a decimal place.
+
+        System.out.println("Firstname: " + firstName); //Displays the first name.
+        System.out.println("Lastname: " + lastName); //Displays the last name.
+        System.out.println("Address: " + address); //Displays the address.
+        System.out.println("Age: " + Age); //Displays the age.
+        System.out.println("GWA: " + gwa); //Displays the gwa.
+
+    } // Ends the main method.
+} // Ends the main class.
